2d17839f28466546eba81e5beb465758bd79f43b
Grant <christiansengrant18@gmail.com>
2024-01-12 23:05:39 -0600

    Added most of the doctests for constraints.rs

--- src/constraints.rs
+++ src/constraints.rs
@@ -2,13 +2,26 @@ use std::collections::HashMap;
 use crate::shunting::{get_legal_variables_iter, ContextHashMap, Token};
 use crate::compile_equation_to_fn_of_hashmap;
 
+/// An enum for indicating why an equation could or could not be added
+/// to a system of equations in a `SystemBuilder`.
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ConstrainResult
 {
+    /// Indicates that the equation added at most 1 unknown variable to 
+    /// the system of equations. 
     WillConstrain,
+
+    /// Indicates that the equation added more than 1 unknown variable
+    /// to the system of equations.
     WillNotConstrain,
+
+    /// Indicates that the equation given will over-constrain the system,
+    /// giving it more equations than degrees of freedom. 
     WillOverConstrain,
 }
 
+/// An object for building up a system of equations and ensuring that it is 
+/// fully constrained prior to attempting to solve it.
 pub struct SystemBuilder<'a>
 {
     context: &'a ContextHashMap,
@@ -17,6 +30,20 @@ pub struct SystemBuilder<'a>
 }
 impl <'a> SystemBuilder<'a>
 {
+    /// Constructs a new `SystemBuilder` instance.
+    /// 
+    /// # Example
+    /// ```
+    /// use geqslib::constraints::SystemBuilder;
+    /// use geqslib::context::{ContextHashMap, ContextLike};
+    /// 
+    /// let mut ctx = ContextHashMap::new();
+    /// ctx.add_var_to_ctx("x", 2.0);
+    /// ctx.add_var_to_ctx("y", 2.0);
+    /// 
+    /// let my_sys = SystemBuilder::new("x + y = 4", &ctx)
+    ///     .expect("failed to build system!");
+    /// ```
     pub fn new(equation: &'a str, ctx: &'a ContextHashMap) -> anyhow::Result<SystemBuilder<'a>>
     {
         Ok(SystemBuilder
@@ -29,7 +56,65 @@ impl <'a> SystemBuilder<'a>
         })
     }
 
-    pub fn try_add_equation(&mut self, equation: &'a str) -> anyhow::Result<ConstrainResult> 
+    /// Gives a reference to the unknown variables in the system.
+    /// 
+    /// # Example
+    /// ```
+    /// use geqslib::constraints::SystemBuilder;
+    /// use geqslib::context::{ContextHashMap, ContextLike};
+    /// 
+    /// let mut ctx = ContextHashMap::new();
+    /// ctx.add_var_to_ctx("x", 2.0);
+    /// ctx.add_var_to_ctx("y", 2.0);
+    /// 
+    /// let my_sys = SystemBuilder::new("x + y = 4", &ctx)
+    ///     .expect("failed to build system!");
+    /// 
+    /// assert_eq!(2, my_sys.get_vars().len());
+    /// assert!(my_sys.get_vars().contains(&"x"));
+    /// assert!(my_sys.get_vars().contains(&"y"));
+    /// ```
+    pub fn get_vars(&self) -> &Vec<&'a str>
+    {
+        &self.system_vars
+    }
+
+    /// Attempts to constrain the system of equations by adding an equation.
+    /// If the equation adds at most 1 unknown variable, it will be added to
+    /// the system and an `Ok(ConstrainResult::WillConstrain)` will be returned.
+    /// If the given equation will over-constrain the system, then an 
+    /// `Ok(ConstrainResult::WillOverConstrain)` is returned. If neither of 
+    /// these happen, but no errors occur during the 
+    /// 
+    /// # Equation
+    /// ```
+    /// use geqslib::constraints::{ConstrainResult, SystemBuilder};
+    /// use geqslib::context::{ContextHashMap, ContextLike};
+    /// 
+    /// let mut ctx = ContextHashMap::new();
+    /// ctx.add_var_to_ctx("x", 1.0);
+    /// ctx.add_var_to_ctx("y", 1.0);
+    /// ctx.add_var_to_ctx("i", 1.0);
+    /// ctx.add_var_to_ctx("j", 1.0);
+    /// 
+    /// let mut my_sys = SystemBuilder::new("x + y = 9", &ctx)
+    ///     .expect("failed to build system!");
+    /// 
+    /// // Too many unknowns to be useful to system.
+    /// let res = my_sys.try_constrain_with("i - j = 4").unwrap();
+    /// assert_eq!(res, ConstrainResult::WillNotConstrain);
+    /// 
+    /// // Adds 0 unknowns and 1 equation. Will not over-constrain
+    /// // the system, and this will not add too many unknowns.
+    /// let res = my_sys.try_constrain_with("x - y = 4").unwrap();
+    /// assert_eq!(res, ConstrainResult::WillConstrain);
+    /// 
+    /// // System is already properly constrained. This will not
+    /// // be useful to add.
+    /// let res = my_sys.try_constrain_with("x - y = 4").unwrap();
+    /// assert_eq!(res, ConstrainResult::WillOverConstrain);
+    /// ```
+    pub fn try_constrain_with(&mut self, equation: &'a str) -> anyhow::Result<ConstrainResult> 
     {
         let mut num_unknowns = 0;
         let mut maybe_new_var = None;
@@ -37,7 +122,7 @@ impl <'a> SystemBuilder<'a>
         let sys_unknowns = self.system_vars.len();
 
         for unknown in get_equation_unknowns(equation, self.context)
-            .filter(|x| self.system_vars.contains(&x))
+            .filter(|x| !self.system_vars.contains(&x))
         {
             num_unknowns += 1;
             maybe_new_var = Some(unknown);
@@ -48,9 +133,11 @@ impl <'a> SystemBuilder<'a>
             // Return early if adding the equation will not gainfully constrain the system
             return Ok(ConstrainResult::WillNotConstrain);
         }
-        else if (sys_equations + 1) > (sys_unknowns + num_unknowns) 
+        else if (sys_equations + 1) > (sys_unknowns + 1) 
+        || self.is_fully_constrained()
         {
-            // Return early if the system will be over-constrained
+            // Return early if the system will be over-constrained or 
+            // no longer fully constrained.
             return Ok(ConstrainResult::WillOverConstrain);
         }
 
@@ -69,20 +156,53 @@ impl <'a> SystemBuilder<'a>
         Ok(ConstrainResult::WillConstrain)
     }
 
-    fn check_constraint(&self) -> bool
+    /// Returns a boolean value indicating whether a system is 
+    /// fully constrained. I.e. the number of equations is equal to
+    /// the number of degrees of freedom.
+    /// 
+    /// # Example
+    /// ```
+    /// use geqslib::constraints::{ConstrainResult, SystemBuilder};
+    /// use geqslib::context::{ContextHashMap, ContextLike};
+    /// 
+    /// let mut ctx = ContextHashMap::new();
+    /// ctx.add_var_to_ctx("x", 1.0);
+    /// ctx.add_var_to_ctx("y", 1.0);
+    /// 
+    /// let mut my_sys = SystemBuilder::new("x + y = 9", &ctx).unwrap();
+    /// 
+    /// assert!(!my_sys.is_fully_constrained());
+    /// 
+    /// my_sys.try_constrain_with("x - y = 4")
+    ///     .expect("failed to constrain more!");
+    /// 
+    /// // Now that we have 2 equations and 2 unknowns, the system is
+    /// // constrained and can be solved.
+    /// assert!(my_sys.is_fully_constrained());
+    /// ```
+    pub fn is_fully_constrained(&self) -> bool
     {
         self.system_equations.len() == self.system_vars.len()
     }
 
-    pub fn try_constraining_with(&mut self, equations: Vec<&'a str>) -> anyhow::Result<()>
+    /// Attempts to fully constrain a system using a given `Vec`
+    /// of equations.
+    /// 
+    /// # Example
+    /// ```
+    /// use geqslib::constraints::SystemBuilder;
+    /// 
+    /// 
+    /// ```
+    pub fn try_fully_constrain_with(&mut self, equations: Vec<&'a str>) -> anyhow::Result<bool>
     {
         let mut still_learning = true;
-        while still_learning && !self.check_constraint()
+        while still_learning && !self.is_fully_constrained()
         {
             still_learning = false;
             for equation in &equations
             {
-                match self.try_add_equation(equation)
+                match self.try_constrain_with(equation)
                 {
                     Ok(ConstrainResult::WillNotConstrain) => {}, 
                     Ok(ConstrainResult::WillConstrain) => {
@@ -97,7 +217,7 @@ impl <'a> SystemBuilder<'a>
                 }
             }
         }
-        Ok(())
+        Ok(self.is_fully_constrained())
     }
 }
 
@@ -105,9 +225,9 @@ pub fn get_equation_unknowns<'a>(equation: &'a str, ctx: &'a ContextHashMap) -> 
 {
     get_legal_variables_iter(equation)
         .filter(|&x| {
-            match ctx[x] 
+            match ctx.get(x) 
             {
-                Token::Var(_) => true,
+                Some(Token::Var(_)) => true,
                 _ => false,
             }
         })
